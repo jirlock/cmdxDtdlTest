87aa616cd6ee740bd5a393a8208b1d08dea7158a
utcmdx23 <utcmdx23@gmail.com>
2024-01-17 12:06:34 +0900

    add dtdl json files

--- request.py
+++ request.py
@@ -1,33 +1,28 @@
 from azure.identity import DefaultAzureCredential
-#from azure.mgmt.digitaltwins import AzureDigitalTwinsManagementClient
 from azure.digitaltwins.core import DigitalTwinsClient
-import os
-import requests
-from requests.auth import HTTPBasicAuth
 
-subscriptionId = "af194cdc-dd36-4e0e-aa3c-0827d9d43f61"
-resourceGroupName = "cmdx"
-resourceName = "15LectureRoom"
-#url = "https://management.azure.com/subscriptions/" + subscriptionId + "/resourceGroups/" + resourceGroupName + "/providers/Microsoft.DigitalTwins/digitalTwinsInstances/" + resourceName + "?api-version=2023-01-31"
+def getTwin(client, name):
+    get_twin = client.get_digital_twin(name)
+    print(get_twin)
+
 url = "https://15LectureRoom.api.jpe.digitaltwins.azure.net"
-
-#sub_id = os.getenv("AZURE_SUBSCRIPTION_ID")
-#client = AzureDigitalTwinsManagementClient(credential=DefaultAzureCredential(), subscription_id=subscriptionId).digital_twins
 
 credential = DefaultAzureCredential()
 client = DigitalTwinsClient(url, credential)
 
-get_twin = client.get_digital_twin("Seats")
-print (get_twin)
-
 patch = [
     {
-        "op": "replace",
-        "path": "/Occupied",
-        "value": 40
+        "op": "add",
+        "path": "/floorInfo",
+        "value": {
+            "maxFloorNumber": 5,
+            "minFloorNumber": -1
+        }
     }
 ]
 
-client.update_digital_twin("Seats", patch)
+twinname = "Eng1"
 
-print(client.get_digital_twin("Seats"))
+client.update_digital_twin(twinname, patch)
+
+getTwin(client, twinname)
